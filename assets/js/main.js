239601f5c16afcc9006e52070997f20014c3654a
Dale Moore <dmoore092@gmail.com>
2019-05-29 20:21:25 -0400

    trying to rotate images for mobile safari

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,31 @@ window.onload = function() {
         return false;
      });
 
-    $("#myCarousel").lightSlider({
+    $("#myCarousel1").lightSlider({
+        item: 4,
+        slideMargin: 10,
+        thumbMargin: 10,
+        autoWidth: true,
+        auto:true,
+        loop: true,
+        slideEndAnimation: true,
+        cssEasing: 'easeinOutBounce',
+        easing: 'linear',
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        pauseOnHover: true,
+        responsive : [{
+            breakpoint:800,
+            settings: {
+            item:1,
+            slideMove:1,
+            slideMargin:6,
+            }
+        }],
+    });
+
+    $("#myCarousel2").lightSlider({
         item: 4,
         slideMargin: 10,
         thumbMargin: 10,
